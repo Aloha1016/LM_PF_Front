2ff851a00a99bb5d68da2c6049ba34d276913444
FabianAl3xis <fa.segovianobalandran@ugto.mx>
2024-11-19 22:26:06 -0600

    Paginas Inventario y Proveedores

---
+++ dueno/Suppliers/form.js
@@ -0,0 +1,17 @@
+const modal = document.getElementById("productModal");
+const addProductBtn = document.getElementById("addProductBtn");
+const closeModal = document.querySelector(".close");
+
+addProductBtn.addEventListener("click", () => {
+    modal.style.display = "flex";
+});
+
+closeModal.addEventListener("click", () => {
+    modal.style.display = "none";
+});
+
+window.addEventListener("click", (event) => {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+});
